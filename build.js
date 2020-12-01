6f0ee544621e4a516382b2ad02142cd8f7e5e0d9
fabiangarga <fabian.gargaglione@gmail.com>
2020-12-01 12:48:45 -0300

    fix(character): character missing

--- build.js
+++ build.js
@@ -2,8 +2,13 @@ var fs = require('fs');
 var path = require('path');
 
 // File "destination.txt" will be created or overwritten by default.
-fs.copyFile(path.resolve(__dirname, 'index.html'), path.resolve(__dirname, 'public/index.html'), (err) => {
-    if (err) 
-        throw err;
-    console.log('FIle copied);
-});
+fs.copyFile(
+    path.resolve(__dirname, 'index.html'), 
+    path.resolve(__dirname, 'public/index.html'), 
+    (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('FIle copied');
+    }
+);
